f33dbcf6483474ceb9c1d8cbfe09c8fc005fcc12
villageworks <villageworks@gmail.com>
2025-12-30 15:25:43 +0900

    feat:SQL文の変更

--- docs/sql/10_master/draft/001_select_reviews.sql
+++ docs/sql/10_master/draft/001_select_reviews.sql
@@ -1,15 +1,14 @@
 SELECT
 	reviews.id
-	, reviews.eatery_id
-	, reviews.reviewer
-	, members.handle
-	, members.name
-	, reviews.title
-	, reviews.comment
-	, reviews.rating
-	, reviews.image
-	, reviews.posted_at
+	, reviews.eatery_id AS eatery_id
+	, reviews.reviewer  AS reviewer_id
+	, members.handle    AS handle
+	, reviews.title     AS title
+	, reviews.comment   AS comment
+	, reviews.rating    AS rating
+	, reviews.image     AS image
+	, reviews.posted_at AS posted_at
 FROM reviews
 JOIN members ON reviews.reviewer = members.id
 WHERE eatery_id = 2
-ORDER BY id;
+ORDER BY posted_at DESC;
